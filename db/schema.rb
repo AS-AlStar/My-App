e30d706dfe397b1bb1a963f9921453b3c5a8b837
AS-AlStar <starovojtov.alexander@gmail.com>
2019-03-22 21:31:07 +0300

    Add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_181825) do
+ActiveRecord::Schema.define(version: 2019_03_22_182826) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
