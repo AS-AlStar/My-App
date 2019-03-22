9c30950f9aceac5b015663662f02387e9cfa676c
AS-AlStar <starovojtov.alexander@gmail.com>
2019-03-23 00:19:17 +0300

    Add relation between categories

---
+++ db/migrate/20190322204557_add_colums_to_category.rb
@@ -0,0 +1,5 @@
+class AddColumsToCategory < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :categories, :parent, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_183659) do
+ActiveRecord::Schema.define(version: 2019_03_22_204557) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
   create_table "category_products", force: :cascade do |t|
