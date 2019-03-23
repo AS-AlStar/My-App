c9f3a098ea9baf64bbce8ffdc293843c474687a8
AS-AlStar <starovojtov.alexander@gmail.com>
2019-03-23 18:39:19 +0300

    Add botstrap and create first pages

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,18 @@ class Category < ApplicationRecord
   has_many :subcategories, class_name: 'Category', foreign_key: :parent_id
 
   belongs_to :parent, class_name: 'Category', optional: true
+
+  def ancestors
+    ancestors = []
+    current_parent = parent
+    while current_parent
+      ancestors.unshift(current_parent)
+      current_parent = current_parent.parent
+    end
+    ancestors
+  end
+
+  def products?
+    products.any?
+  end
 end
