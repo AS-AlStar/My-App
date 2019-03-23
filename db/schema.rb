2f64f1ac4a9569541a7efe54cbc8442370f306ca
AS-AlStar <starovojtov.alexander@gmail.com>
2019-03-23 03:08:18 +0300

    Add url to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_204557) do
+ActiveRecord::Schema.define(version: 2019_03_22_230526) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_204557) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
 end
